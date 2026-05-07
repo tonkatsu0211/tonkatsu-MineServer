dc04fde61b1efa32e1c6346722efd99f7f9118c0
CodeSandbox <support@codesandbox.io>
2026-05-07 11:14:45 +0000

    Sandbox snapshot commit

--- eaglerproxy-1.1.0/example_plugins/AutokickPlugin/config.js
+++ eaglerproxy-1.1.0/example_plugins/AutokickPlugin/config.js
@@ -1,4 +1,4 @@
 export const config = {
-    bindInternalServerPort: 25569,
-    bindInternalServerIp: "127.0.0.1",
+  bindInternalServerPort: 25565,
+  bindInternalServerIp: "127.0.0.1",
 };
--- eaglerproxy-1.1.0/src/plugins/EagProxyAAS/config.js
+++ eaglerproxy-1.1.0/src/plugins/EagProxyAAS/config.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.config = void 0;
 exports.config = {
-    bindInternalServerPort: 25569,
+    bindInternalServerPort: 25565,
     bindInternalServerIp: "127.0.0.1",
     allowCustomPorts: true,
     allowDirectConnectEndpoints: true,
